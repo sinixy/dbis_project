8faea73461173943695e24bb7521df42ff080b6a
sinixy <sinixyowo@gmail.com>
2021-12-05 19:37:40 +0200

    tests init

--- kpi_network/test_flaskr.py
+++ kpi_network/test_flaskr.py
@@ -3,38 +3,17 @@ import tempfile
 
 import pytest
 
-from flaskr import create_app
-from flaskr.db import init_db
+from kpi_network import app
 
+
+app.config['TESTING'] = True
 
 @pytest.fixture
 def client():
-    db_fd, db_path = tempfile.mkstemp()
-    app = create_app({'TESTING': True, 'DATABASE': db_path})
+    client = app.test_client()
+    return client
 
-    with app.test_client() as client:
-        with app.app_context():
-            init_db()
-        yield client
-
-    os.close(db_fd)
-    os.unlink(db_path)
-
-
-def test_login_logout(client):
-    """Make sure login and logout works."""
-
-    username = flaskr.app.config["USERNAME"]
-    password = flaskr.app.config["PASSWORD"]
-
-    rv = login(client, username, password)
-    assert b'You were logged in' in rv.data
-
-    rv = logout(client)
-    assert b'You were logged out' in rv.data
-
-    rv = login(client, f"{username}x", password)
-    assert b'Invalid username' in rv.data
-
-    rv = login(client, username, f'{password}x')
-    assert b'Invalid password' in rv.data
+def test_login(client):
+    data = client.get('/user/1').data
+    print(data)
+    assert 'id' in data.keys()
