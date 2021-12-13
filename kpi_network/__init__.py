b7e525848e45c19426726f366472075ff04ac1d1
sinixy <sinixyowo@gmail.com>
2021-12-14 01:06:57 +0200

    fixed database uri

--- kpi_network/__init__.py
+++ kpi_network/__init__.py
@@ -6,7 +6,11 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
+uri = os.environ['DATABASE_URL']
+if uri and uri.startswith("postgres://"):
+	uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['BASE_URL'] = os.environ['BASE_URL']
 # app.secret_key = b'...'
